ff64ffc8bfc1fa88c8531367dfa3384c18a5bd9f
CamilaMandai <cayumandai@gmail.com>
2023-07-03 10:40:45 -0300

    chore: instala vue-route, configura rota

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
+    component: Home,
   }
 ]
 
