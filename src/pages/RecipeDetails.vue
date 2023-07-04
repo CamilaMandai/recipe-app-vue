1a41caa2317703b92062d940f78e3f56ca541a37
CamilaMandai <cayumandai@gmail.com>
2023-07-04 10:42:50 -0300

    fix: erro de reatividade em RecipeDetails

--- src/pages/RecipeDetails.vue
+++ src/pages/RecipeDetails.vue
@@ -1,13 +1,13 @@
 <template>
   <routerLink to="/"><i class="material-icons mt-2">home</i></routerLink>
 
-  <div class="card mt-5 border-0 mx-auto" style="width: 50%;">
+  <div class="card mt-5 border-0 mx-auto">
     <div class="card-body">
-      <img class="card-img-top rounded" :src="selectedRecipe.strMealThumb" :alt="selectedRecipe.strMeal" />
+      <img class="card-img-top rounded" :src="recipeStore.selectedRecipe.strMealThumb" :alt="recipeStore.selectedRecipe.strMeal" />
       <div class="d-flex justify-content-between">
-        <h2 class="card-title mt-2">{{ selectedRecipe.strMeal }}</h2>
-        <i class="material-icons mt-2" :class="{ 'text-danger': recipeStore.isFavorite(selectedRecipe.idMeal) }"
-          @click="recipeStore.toggleFav(selectedRecipe)">
+        <h2 class="card-title mt-2">{{ recipeStore.selectedRecipe.strMeal }}</h2>
+        <i class="material-icons mt-2" :class="{ 'text-danger': recipeStore.isFavorite(recipeStore.selectedRecipe.idMeal) }"
+          @click="recipeStore.recipeStore.toggleFav(recipeStore.selectedRecipe)">
           favorite
         </i>
       </div>
@@ -15,8 +15,8 @@
       <div>
         <ul>
           <template v-for="(el, index) of new Array(20)">
-            <li v-if="selectedRecipe[`strMeasure${index+1}`]">
-              {{ selectedRecipe[`strMeasure${index+1}`]}} {{ selectedRecipe[`strIngredient${index+1}`] }}
+            <li v-if="recipeStore.selectedRecipe[`strMeasure${index+1}`]">
+              {{ recipeStore.selectedRecipe[`strMeasure${index+1}`]}} {{ recipeStore.selectedRecipe[`strIngredient${index+1}`] }}
             </li>
           </template>
         </ul>
@@ -26,7 +26,7 @@
         <!-- <template v-for="el of selectedRecipe.strInstructions.split('.')">
           <p>{{ el }}.</p>
         </template> -->
-        <p>{{ selectedRecipe.strInstructions }}</p>
+        <p>{{ recipeStore.selectedRecipe.strInstructions }}</p>
        </div>
     </div>
   </div>
@@ -41,12 +41,11 @@ import { useRecipeStore } from '../store/RecipeStore';
 
 const route = useRoute();
 const recipeStore = useRecipeStore();
-const { selectedRecipe } = recipeStore;
 
 onMounted(async () => {
   try {
     await recipeStore.getOneRecipe(route.params.id);
-    console.log(selectedRecipe.strMeal + 'loaded')
+    console.log(recipeStore.selectedRecipe.strMeal + ' ' + 'loaded')
   } catch (e) { 
     console.log(e)
   } 
